fb377c753924b8ba519272467115e7aab898d63b
Mathieu Minet <mminet@c1r2p9.42lehavre.fr>
2025-10-21 17:28:46 +0200

    [dev] ajout map

--- includes/lem-ipc.h
+++ includes/lem-ipc.h
@@ -8,10 +8,26 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <semaphore.h>
 
 # define MAP_WIDTH 20
 # define MAP_HEIGHT 20
-# define SHM_KEY_PATH "shmfile"
+# define MAP_SIZE MAP_HEIGHT * MAP_WIDTH
+# define SHM_KEY_PATH_MAP "/tmp"
+# define SHM_KEY_PATH_SEM "/tmp"
 # define SHM_KEY_ID 65
+
+typedef struct	s_lem_ipc
+{
+
+	sem_t		*semaphore;
+	int			semId;
+	key_t		semKey;
+	int			*map;
+	int			mapId;
+	key_t		mapKey;
+	int			is_creator;
+}				t_lem_ipc;
+
 
 #endif
--- src/lem-ipc.c
+++ src/lem-ipc.c
@@ -1,56 +1,110 @@
 #include "../includes/lem-ipc.h"
 
-int main()
+void	printMap(int *map)
 {
-    key_t key = ftok(SHM_KEY_PATH, SHM_KEY_ID);
-	int shmid;
-	int is_creator = 0;
+	int x = 0;
+	int y = 0;
 
-    shmid = shmget(key, 1024, 0666 | IPC_CREAT | IPC_EXCL);
-	if (shmid == -1)
+	while (x < MAP_HEIGHT)
 	{
-		printf("c'est pas le premier !\n");
-		if (errno == EEXIST)
+		while (y < MAP_WIDTH)
 		{
-			shmid = shmget(key, 1024, 0666);
-			if (shmid == -1)
-			{
-				printf("shmget error\n");
-				return (1);
-			}
+			if (y != 0)
+				printf("%2d", map[x * MAP_WIDTH + y]);
+			else
+				printf("%2d", map[x * MAP_WIDTH + y]);
+			y++;
 		}
-		else
-		{
-			printf("shmget error\n");
-			return (1);
-		}
+		printf("\n");
+		y = 0;
+		x++;
 	}
-	else
-	{
-		is_creator = 1;
-	}
+}
 
-	char *str = (char*) shmat(shmid, NULL, 0);
-	if (str == (char *)(-1))
+int		shm_init(t_lem_ipc *all)
+{
+	all->semId = shmget(all->semKey, sizeof(sem_t *), 0666);
+	all->semaphore = (sem_t *) shmat(all->semId, NULL, 0);
+	if (!all->semaphore)
 	{
-		perror("shmat");
+		perror("Error semaphore\n");
 		return (1);
 	}
-	if (is_creator)
+	//sem_wait(all->semaphore);
+	all->mapId = shmget(all->mapKey, MAP_SIZE * sizeof(int), 0666);
+	if (all->mapId == -1)
 	{
-		printf("Je suis le premier !\n");
-		strcpy(str, "Hello from creator process");
-		printf("Donnee ecrite\n");
-		sleep(10);
-		shmdt(str);
-		shmctl(shmid, IPC_RMID, NULL);
-		printf("memoire supprimer !\n");
+		perror("Error shmget\n");
+		return (1);
+	}
+	all->map = (int *)shmat(all->mapId, NULL, 0);
+	if (all->map == (int *)(-1))
+	{
+		perror("shmat");
+		shmdt(all->map);
+		shmctl(all->mapId, IPC_RMID, NULL);
+		return (1);
+	}
+	//printMap(all->map);
+	//sem_post(all->semaphore);
+	return (0);
+}
+
+int		first_init(t_lem_ipc *all)
+{
+	all->semId = shmget(all->semKey, sizeof(sem_t), 0666 | IPC_CREAT | IPC_EXCL);
+	all->mapId = shmget(all->mapKey, MAP_SIZE * sizeof(int), 0666 | IPC_CREAT | IPC_EXCL);
+	if (all->mapId == -1)
+		printf("%d\n", errno);
+	all->is_creator = 1;
+	all->semaphore = (sem_t *)shmat(all->semId, NULL, 0);
+	if (!all->semaphore)
+	{
+		perror("Error semaphore\n");
+		return (1);
+	}
+	//sem_init(all->semaphore, 1, 1);
+	//sem_wait(all->semaphore);
+	if (all->mapId == -1)
+	{
+		perror("Error shmget\n");
+		return (1);
+	}
+	all->map = (int *)shmat(all->mapId, NULL, 0);
+	if (all->map == (int *)(-1))
+	{
+		perror("shmat");
+		shmdt(all->map);
+		shmctl(all->mapId, IPC_RMID, NULL);
+		return (1);
+	}
+	for (int a = 0; a < MAP_SIZE; a++)
+		all->map[a] = 0;
+	printMap(all->map);
+	//sem_post(all->semaphore);
+	return (0);
+}
+
+int main()
+{
+	t_lem_ipc	all;
+
+	all.mapKey = ftok(SHM_KEY_PATH_MAP, SHM_KEY_ID);
+	all.semKey = ftok(SHM_KEY_PATH_SEM, SHM_KEY_ID + 1);
+	all.is_creator = 0;
+	all.semId = shmget(all.semKey, sizeof(sem_t), 0666 | IPC_CREAT | IPC_EXCL);
+	all.mapId = shmget(all.mapKey, MAP_SIZE * sizeof(int), 0666 | IPC_CREAT | IPC_EXCL);
+	if (all.semId != -1 || all.mapId != -1)
+	{
+		if (all.mapId != -1)
+			shmctl(all.mapId, IPC_RMID, NULL);
+		if (all.semId != -1)
+			shmctl(all.semId, IPC_RMID, NULL);
+		first_init(&all);
 	}
 	else
-	{
-		sleep (1);
-		printf("[Lecteur] Donnée lue depuis la mémoire : %s\n", str);
-		shmdt(str);
-	}
-    return 0;
+		shm_init(&all);
+	sleep(5);
+	shmctl(all.mapId, IPC_RMID, NULL);
+	shmctl(all.semId, IPC_RMID, NULL);
 }
